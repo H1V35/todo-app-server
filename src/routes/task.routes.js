4fd6f07535b7b01d9a7566f6ecd9745fb6b7354e
H1V35 <higesjorge@gmail.com>
2024-03-31 19:43:43 +0200

    🐛 fix: Solve CORS error, server now accessible from any URL; Refactor routes

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -7,14 +7,14 @@ import {
   deleteCompletedTasks,
 } from "../controllers/task.controller.js";
 
-export const router = Router();
+export const tasksRouter = Router();
 
-router.get("/tasks", getTasks);
+tasksRouter.get("/", getTasks);
 
-router.post("/tasks", createTask);
+tasksRouter.post("/", createTask);
 
-router.post("/tasks/:task_id/complete", toggleCompleteTask);
+tasksRouter.post("/:task_id/complete", toggleCompleteTask);
 
-router.delete("/tasks/:task_id/delete", deleteTask);
+tasksRouter.delete("/:task_id/delete", deleteTask);
 
-router.delete("/tasks/delete-completed", deleteCompletedTasks);
+tasksRouter.delete("/delete-completed", deleteCompletedTasks);
